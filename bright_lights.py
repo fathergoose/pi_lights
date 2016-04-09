a86fd6f22bbc31ceb5f796c14fac4397fe3854f6
fathergoose <al@ilseman.com>
2016-04-09 23:02:31 +0000

    oh my god it works

--- bright_lights.py
+++ bright_lights.py
@@ -1,7 +1,9 @@
 import RPi.GPIO as GPIO
 import time
+import resistance
 
 led_pin = 4
+CALIBRATION = 300 # maximum MEASURED value of resistance.read_pot()
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(led_pin, GPIO.OUT)
@@ -16,12 +18,17 @@ def blink(times, speed):
         GPIO.output(led_pin, False)
         time.sleep(speed)
         i = i + 1
-    #GPIO.cleanup()
 
 # blink(5, .2)
 
 while True:
-    duty_s = input("% Brightness? ")
-    duty = int(duty_s)
+    pot = resistance.read_pot()
+    duty = pot // 10
     pwm_led.ChangeDutyCycle(duty)
+    time.sleep(1)
+    
+    # duty_s = input("% Brightness? ")
+    # duty = int(duty_s)
+    # pwm_led.ChangeDutyCycle(duty)
 
+GPIO.cleanup()
